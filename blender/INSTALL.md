8c350180dbd7dccb9c57eede161e4c7a19c7b5a0
Erik Ogenvik <erik@ogenvik.org>
2018-04-03 22:39:43 +0200

    Combine all into one file, to make install easier.

--- blender/INSTALL.md
+++ blender/INSTALL.md
@@ -4,12 +4,11 @@ WorldForge Pipeline
 REQUIREMENTS:
 	Ogre exporter http://code.google.com/p/blender2ogre
 
-INSTALL: 
-	Install by going to addons 
+INSTALL:
+	Install by going to addons
 		--> Crt-Alt-U
 		--> Click on "Install From File"
 			--> Select wf_pipeline_tools.py for the tools
-			--> Select wf_tool_panel.py for the panel
 
 	New WorldForge Tab will be created. 
 		--> Enable Addons
--- blender/wf_pipeline_tools.py
+++ blender/wf_pipeline_tools.py
@@ -589,7 +589,107 @@ def wf_pivot_to_selected_manual_map():
 
 
 # ----------------------------------------------------------------------------
-# --------------------------- REGISRATION ------------------------------------
+# --------------------------- PANEL ------------------------------------------
+# ----------------------------------------------------------------------------
+
+bpy.types.Scene.Rig = bpy.props.StringProperty()
+
+
+def get_armature(name):
+    '''gets the name of the current armature '''
+    for ob in bpy.data.objects:
+        if ob.name == name:
+            return ob
+    return False
+
+
+class PANEL_OT_wf_tools(bpy.types.Panel):
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "WorldForge"
+    bl_label = "Tools"
+    bl_options = {"DEFAULT_CLOSED"}
+
+    def draw(self, context):
+        scene = context.scene
+        layout = self.layout
+
+        col = layout.column(align=True)
+        col.label(text="Rename Objects:")
+        layout.prop(scene, "wf_rename_panel")
+        row = layout.row()
+        row.operator('object.wf_rename_objects', text='Renamer', icon="FILE_TICK")
+        col = layout.column(align=True)
+        col.operator('object.wf_pivot_to_selected', text='', icon='FORCE_FORCE')
+        row = col.row(align=True)
+        row.operator("object.shade_smooth", text="Smooth")
+        row.operator("object.shade_flat", text="Flat")
+
+
+class PANEL_OT_wf_mat_panel(bpy.types.Panel):
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "WorldForge"
+    bl_label = "Material Utils"
+    bl_options = {"DEFAULT_CLOSED"}
+
+    def draw(self, context):
+        scene = context.scene
+        layout = self.layout
+
+        row = layout.row()
+        # layout.qlabel(text="Material Utilities")
+        row = layout.row(align=True)
+        row.operator('mesh.wf_fix_materials', text='Fix Materials', icon='SCULPTMODE_HLT')
+        row.operator('scene.wf_open_ogre_materials', text='Ogre Mats', icon='IMASEL')
+        # col = layout.column(align=True)
+        row = layout.row(align=True)
+        row.operator('view3d.material_to_texface', text='Mat to Tex', icon='MATERIAL_DATA')
+        row.operator('view3d.texface_to_material', text='Tex to Mat', icon='FACESEL_HLT')
+
+
+class PANEL_OT_wf_rigging_panel(bpy.types.Panel):
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "WorldForge"
+    bl_label = "Rigging Utils"
+    bl_options = {"DEFAULT_CLOSED"}
+
+    def draw(self, context):
+        scene = context.scene
+        layout = self.layout
+        col = layout.row()
+        col.operator('object.vertex_group_limit_total', text='Limit Weights')
+        col.operator('object.clean_vertex_groups', text='Clean Weights')
+
+
+class PANEL_OT_wf_ogre_export(bpy.types.Panel):
+    """Worldforge Tools Panel"""
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+    bl_category = "WorldForge"
+    # bl_context = "objectmode"
+    bl_label = "Mesh Export"
+    bl_options = {"DEFAULT_CLOSED"}
+
+    def draw(self, context):
+        layout = self.layout
+
+        scene = context.scene
+
+        row = layout.row()
+        row.operator("mesh.wf_export_ogre_static", icon='VIEW3D')
+
+        row = layout.row(align=True)
+        row.prop(scene, "frame_start")
+        row.prop(scene, "frame_end")
+        row = layout.row()
+        row.prop(scene, "EX_wf_export_optimize")
+        row = layout.row()
+        row.operator("mesh.wf_export_ogre_animated", icon='BONE_DATA')
+
+# ----------------------------------------------------------------------------
+# --------------------------- REGISTRATION -----------------------------------
 # ----------------------------------------------------------------------------
 def register():
     bpy.types.Scene.wf_rename_panel = bpy.props.StringProperty(name="", description="Rename objects with this string")
@@ -615,6 +715,12 @@ def register():
     bpy.utils.register_class(OBJECT_OT_wf_pivot_to_selected)
     bpy.utils.register_manual_map(wf_pivot_to_selected_manual_map)
 
+    bpy.utils.register_class(PANEL_OT_wf_ogre_export)
+    bpy.utils.register_class(PANEL_OT_wf_mat_panel)
+    bpy.utils.register_class(PANEL_OT_wf_rigging_panel)
+    bpy.utils.register_class(PANEL_OT_wf_tools)
+    bpy.types.Scene.EX_wf_export_optimize = bpy.props.BoolProperty(default=False, name="Optimize mesh",
+                                                                   description="If enabled, MeshMagick (if available) will be used to optimize the mesh.")
 
 def unregister():
     del bpy.types.Scene.wf_rename_panel
@@ -640,6 +746,10 @@ def unregister():
     bpy.utils.unregister_class(OBJECT_OT_wf_pivot_to_selected)
     bpy.utils.unregister_manual_map(wf_pivot_to_selected_manual_map)
 
+    bpy.utils.unregister_class(PANEL_OT_wf_ogre_export)
+    bpy.utils.unregister_class(PANEL_OT_wf_mat_panel)
+    bpy.utils.unregister_class(PANEL_OT_wf_rigging_panel)
+    bpy.utils.unregister_class(PANEL_OT_wf_tools)
 
 if __name__ == '__main__':
     register()
